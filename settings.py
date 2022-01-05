4535de82b8fc58b78dd5bc54df41504f43c67b9b
Kushnaryov <mkushnarov@gmail.com>
2022-01-05 19:31:34 +0100

    db url

--- settings.py
+++ settings.py
@@ -10,4 +10,4 @@ content_dir_name = 'static/media/content'
 app_dir = os.path.realpath(os.path.dirname(__file__))
 content_path = os.path.join(app_dir, content_dir_name)
 db_path = os.path.join(app_dir, DATABASE_FILE)
-SQLALCHEMY_DATABASE_URI = 'postgres://sodluwekofgiyz:4bcb5a2885a5b3613a650bac01ab565d5c33ede79120649ef2bc3dc0e7548e58@ec2-54-220-223-3.eu-west-1.compute.amazonaws.com:5432/dd7fn5k9enmm1m'
+SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
