71edcf1b7ffe9cc14595f92dd10179724cdc4b16
Kushnaryov <mkushnarov@gmail.com>
2022-01-18 21:55:21 +0100

    filters added

--- main_app/settings.py
+++ main_app/settings.py
@@ -10,4 +10,4 @@ app_dir = os.path.realpath(os.path.dirname(__file__))
 # db_path = os.path.join(app_dir, DATABASE_FILE)
 
 DEV_DB_URI = 'postgresql://postgres:joydivision32@localhost/RudolfKiss'
-PROD_DB_URI = 'postgresql://sodluwekofgiyz:4bcb5a2885a5b3613a650bac01ab565d5c33ede79120649ef2bc3dc0e7548e58@ec2-54-220-223-3.eu-west-1.compute.amazonaws.com:5432/dd7fn5k9enmm1m'
+PROD_DB_URI = 'postgresql://kmwxpdyyexwybk:bfb5b1676e82a15d198ab2f38b8f05314a0fa4defda98f542fe2636b68859aaf@ec2-34-249-49-9.eu-west-1.compute.amazonaws.com:5432/d6vpkslpcd5ad4'
