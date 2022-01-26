ed0b217e51b481fc82da4ae80bdaf4a5ccee8f38
Kushnaryov <mkushnarov@gmail.com>
2022-01-26 19:34:20 +0100

    embed code corrected

--- admin_app/helpers.py
+++ admin_app/helpers.py
@@ -91,7 +91,7 @@ def get_name(url: str):
     return name
 
 def get_embed_url(url: str):
-    return 'https://player.vimeo.com/video/'+url[-9:]
+    return 'https://player.vimeo.com/video/'+re.find('\d*)')
 
 def delete_mp4(path: str, filename: str, usage: str):
     try:
