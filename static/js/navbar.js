069bf59df2a2e96bee3a47ce4fb9c87723691058
Kushnaryov <mkushnarov@gmail.com>
2022-02-03 17:00:47 +0100

    mobile menu added

--- static/js/navbar.js
+++ static/js/navbar.js
@@ -11,6 +11,7 @@ $(document).ready(function() {
             $('#nav').addClass('fixed');
             $('#cross').removeClass('cross-not-fixed')
             $('#cross').height('7vh')
+            $('.cross').addClass('cross-transf')
             $('#filter-name').removeClass('name-not-fixed')
             $('#filter-comm').text('COMMERCIALS')
             $('#filter-comm').addClass('fs-name-s')
@@ -18,7 +19,9 @@ $(document).ready(function() {
             $('#filter-doc').text('DOCUMENTARIES')
             $('#filter-shf').text('SHORT FILMS')
             $('#filter-dop').text('DOP WORKS')
-            
+            $('.menu-button').each (function () {
+                $(this).addClass('menu-show')
+            });
             // filter-mus
             $('#background').css('display', 'none')
             $('.video-name').css('display', 'none')
@@ -26,6 +29,7 @@ $(document).ready(function() {
             $('#nav').removeClass('fixed');
             $('#cross').addClass('cross-not-fixed')
             $('#cross').height(cross)
+            $('.cross').removeClass('cross-transf')
             $('#filter-name').addClass('name-not-fixed')
             $('#filter-comm').text('KISS')
             $('#filter-comm').removeClass('fs-name-s')
@@ -35,6 +39,9 @@ $(document).ready(function() {
             $('#filter-dop').text('')
             $('#background').css('display', 'flex')
             $('.video-name').css('display', 'inline-flex')
+            $('.menu-button').each (function () {
+                $(this).removeClass('menu-show')
+            });
         };
 
         if ($(window).scrollTop() >= top) {
@@ -45,6 +52,44 @@ $(document).ready(function() {
         };
     });
 });
+
+// MENU FOR MOBILE VERSION
+$(document).ready(function(){
+    var previousScrollTop = 0, scrollLock = false;
+    // $(window).scroll(function(event) { 
+      
+    //   if(scrollLock) {
+    //     $(window).scrollTop(previousScrollTop); 
+    //   }
+    //   previousScrollTop = $(window).scrollTop();
+  
+    //   console.log(scrollLock)
+    // });
+    $('.menu').hide();
+  
+  
+    $(".menu-button").click(function() {
+      $('.menu').fadeIn("fast");
+      scrollLock = true;
+      $(".burger-bar").css('background', '#fdbebe');
+      $(".burger-bar:first-child").css('-webkit-transform', 'rotate(-135deg) translate(-2px, -1px)');
+      $(".burger-bar:nth-child(2)").addClass('turn-off')
+      $(".burger-bar:nth-child(3)").css('-webkit-transform', 'rotate(-45deg) translate(2px, -1px)');
+      $(".menu-header").addClass('menu-show')
+      $('#nav').fadeOut("fast");
+    }); 
+    $(".close-button").click(function() {
+      
+      scrollLock = false;
+      $(".burger-bar").css('background', 'white');
+      $(".burger-bar:first-child").css('-webkit-transform', 'translateY(-10px)');
+      $(".burger-bar:nth-child(2)").removeClass('turn-off')
+      $(".burger-bar:nth-child(3)").css('-webkit-transform', 'translateY(10px)');
+      $(".menu-header").removeClass('menu-show')
+      $('.menu').fadeOut("fast");
+      $('#nav').fadeIn("fast");
+    });
+  });
 // Get the navbar
 // var navbar = $("#nav");
 
